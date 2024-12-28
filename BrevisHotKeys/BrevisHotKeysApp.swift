da7ddc9e1c6c4ee3bbf4c42bd3430c5f96ab41b3
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-12-28 10:29:35 +0200

    Initial Commit

---
+++ BrevisHotKeys/BrevisHotKeysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrevisHotKeysApp.swift
+//  BrevisHotKeys
+//
+//  Created by Manyuchi, Carrington C on 2024/12/28.
+//
+
+import SwiftUI
+
+@main
+struct BrevisHotKeysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
